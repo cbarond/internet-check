dd1690c4f874d92223794fae01ec0d2c3c22f1c5
Caelan Barondess <cbarondess19@gmail.com>
2022-08-03 19:42:09 -0600

    Add path variable and untrack data files

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Runs a speedtest at predefined intervals and records when internet is lost
 To use:
 
 1. Clone repo or download and extract .zip
-2. Run internet-check.py
-3. Insert time between tests and start offset in hours
-4. Check speed.csv for speed test results
-5. Check no_internet.txt for log of internet outages
+2. Change datapath variable to the folder path of the data files
+3. Run internet-check.py
+4. Insert time between tests and start offset in hours
+5. Check speed.csv for speed test results
+6. Check no_internet.txt for log of internet outages
 
--- internet-check.py
+++ internet-check.py
@@ -5,46 +5,79 @@ from datetime import datetime, time
 import csv
 import schedule
 import threading
-from alive_progress import alive_bar
-from alive_progress.styles import showtime
+#from alive_progress import alive_bar
+#from alive_progress.styles import showtime
 from halo import Halo
 
+# Insert path to data files here
+datapath = r""
+text_file = rf'{datapath}\no_internet.txt'
+speed_file = rf'{datapath}\speed.csv'
+
+print(text_file)
+print(speed_file)
+
+# Initialize data files
+with open(text_file, "a") as myfile:
+    pass
+with open(speed_file, 'a', newline='') as f:
+    pass
+
+internet = True
 
 def internet_on():
     try:
         response = requests.get("http://google.com")
-
         if response.ok:
             return True
         else:
             return True
     except:
-        current_time = datetime.now()
-        with open("no_internet.txt", "a") as myfile:
-            myfile.write(f"No internet @ {current_time}\n")
+        
+        return False
+
+def constant_check():
+    global internet
+    current_time = datetime.now()
+    current_status = internet_on()
+    if (current_status != internet) & internet_on():
+        print("\n", "-"*40)
+        print(f"Internet Returns @ {current_time}")
+        print("-"*40)
+        internet = True
+
+        with open(text_file, "a") as myfile:
+            myfile.write(f"Internet Returns @ {current_time}\n")
+
+
+    elif (current_status != internet) & (not internet_on()):
         print("\n", "-"*40)
         print(f"No internet @ {current_time}")
         print("-"*40)
-        return False
+        internet = False
+
+        with open(text_file, "a") as myfile:
+            myfile.write(f"No internet @ {current_time}\n")
 
 def speed_check():
     current_time = datetime.now()
     if internet_on():
         start = t.time()
         st = speedtest.Speedtest()
+        st.get_best_server()
+        download = st.download()/1000000
+        upload = st.upload()/1000000
         end = t.time()
         speed_dur = end - start
 
         start = t.time()
-        fields = [current_time,st.download()/1000000,st.upload()/1000000]
-        with open(r'speed.csv', 'a', newline='') as f:
+        fields = [current_time,download,upload]
+        with open(speed_file, 'a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(fields)
         end = t.time()
         write_dur = end - start
 
-        download = st.download()/1000000
-        upload = st.upload()/1000000
 
         print("\n", "-"*40)
         print(current_time)
@@ -61,7 +94,7 @@ def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
-hours = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+hours = ['00:05', '01:05', '02:05', '03:05', '04:05', '05:05', '06:05', '07:05', '08:05', '09:05', '10:05', '11:05', '12:05', '13:05', '14:05', '15:05', '16:05', '17:05', '18:05', '19:05', '20:05', '21:05', '22:05', '23:05']
 
 def schedule_test():
     delay = int(input("Time between tests: "))
@@ -92,11 +125,14 @@ def test():
     #    myfile.write(f"Testing")
     print("\nhello")
 
-schedule.every(30).seconds.do(run_threaded, internet_on)
+#schedule.every(30).seconds.do(run_threaded, internet_on)
+schedule.every(5).seconds.do(run_threaded, constant_check)
+
 #schedule.every().day.at("00:00").do(run_threaded, status)
 #schedule.every(3).seconds.do(run_threaded, status)
 
-#schedule.every().day.at("19:45").do(run_threaded, speed_check)
+#time = ['08:56']
+#schedule.every().day.at(time[0]).do(run_threaded, speed_check)
 
 #schedule.every().day.at("00:00").do(run_threaded, speed_check)
 #schedule.every().day.at("03:00").do(run_threaded, speed_check)
@@ -121,3 +157,7 @@ def run():
         t.sleep(1)
 
 run()
+
+
+# TODO: make internet check func run every 5 sec and only record when state flips
+# TODO: add path input and store data outside git folder
