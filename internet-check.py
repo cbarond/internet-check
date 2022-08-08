1681e3cf326719a5e96d64b71910168d642c9c7c
Caelan Barondess <cbarondess19@gmail.com>
2022-08-07 18:08:39 -0600

    Changed tracking function and added colors

--- internet-check.py
+++ internet-check.py
@@ -5,17 +5,19 @@ from datetime import datetime, time
 import csv
 import schedule
 import threading
+from rich.console import Console
 #from alive_progress import alive_bar
 #from alive_progress.styles import showtime
-from halo import Halo
+#from halo import Halo
+
+console = Console()
 
 # Insert path to data files here
-datapath = r""
+datapath = r"C:\Users\Caelan\Documents\Programming\Python\internet-check"
 text_file = rf'{datapath}\no_internet.txt'
 speed_file = rf'{datapath}\speed.csv'
 
-print(text_file)
-print(speed_file)
+print(f"Data files: \n{text_file} \n{speed_file}")
 
 # Initialize data files
 with open(text_file, "a") as myfile:
@@ -34,31 +36,36 @@ def internet_on():
             return True
     except:
         current_time = datetime.now()
-        with open(text_file, "a") as myfile:
-            myfile.write(f"No internet @ {current_time}\n")
+        #with open(text_file, "a") as myfile:
+        #    myfile.write(f"No internet @ {current_time}\n")
         return False
 
 def constant_check():
     global internet
+    line = "-"*45
     current_time = datetime.now()
     current_status = internet_on()
-    if (current_status != internet) & (internet_on() is True):
+    if internet == current_status:
+        return
+    elif (internet is False) & (current_status is True):
+        print(f"\n{line}")
+        #print(f"Internet: {internet}", flush=True)
         internet = True
-        print("\n", "-"*40, flush=True)
-        print(f"Internet: {internet}", flush=True)
-        print(f"Internet Returns @ {current_time}", flush=True)
-        print("-"*40, flush=True)
+        console.print(f"Internet Returns @ {current_time}", style="green")
+        #print(f"Internet: {internet}")
+        print(f"{line}", flush=True)
 
         with open(text_file, "a") as myfile:
             myfile.write(f"Internet Returns @ {current_time}\n")
 
 
-    elif (current_status != internet) & (internet_on() is False):
+    elif (internet is True) & (current_status is False):
+        print(f"\n{line}")
+        #print(f"Internet: {internet}", flush=True)
         internet = False
-        print("\n", "-"*40, flush=True)
-        print(f"Internet: {internet}", flush=True)
-        print(f"No internet @ {current_time}", flush=True)
-        print("-"*40, flush=True)
+        console.print(f"No internet @ {current_time}", style="red")
+        #print(f"Internet: {internet}")
+        print(f"{line}", flush=True)
 
         with open(text_file, "a") as myfile:
             myfile.write(f"No internet @ {current_time}\n")
@@ -82,27 +89,30 @@ def speed_check():
         end = t.time()
         write_dur = end - start
 
+        line = "-"*45
 
-        print("\n", "-"*40)
+        print(f"\n{line}")
         print(current_time)
-        print(f"Download: {download}\nUpload: {upload}")
+        print(f"[cyan]Download: {download}[/]\n[purple]Upload: {upload}[/]")
         print(f"Speedtest Duration: {speed_dur}")
         print(f"Write Duration: {write_dur}")
-        print("-"*40)
+        print(f"{line}")
     else:
-        print("\n", "-"*40)
+        print(f"\n{line}")
         print(f"No internet @ {current_time}")
-        print("-"*40)
+        print(f"{line}")
 
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
+
+# TODO add option to choose minute offset
 hours = [
-    '00:05', '01:05', '02:05', '03:05', '04:05', '05:05', 
-    '06:05', '07:05', '08:05', '09:05', '10:05', '11:05', 
-    '12:05', '13:05', '14:05', '15:05', '16:05', '17:05', 
-    '18:05', '19:05', '20:05', '21:05', '22:05', '23:05'
+    '00:15', '01:15', '02:15', '03:15', '04:15', '05:15', 
+    '06:15', '07:15', '08:15', '09:15', '10:15', '11:15', 
+    '12:15', '13:15', '14:15', '15:15', '16:15', '17:15', 
+    '18:15', '19:15', '20:15', '21:15', '22:15', '23:15'
 ]
 
 def schedule_test():
@@ -114,11 +124,11 @@ def schedule_test():
         schedule.every().day.at(hours[i]).do(run_threaded, speed_check)
         i += delay
 
-def status():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    spinner = Halo(text='Running', spinner={"interval": 80,	"frames": ["⠋","⠙","⠹","⠸","⠼","⠴","⠦","⠧","⠇","⠏"]})
-    spinner.start()
+#def status():
+#    now = datetime.now()
+#    current_time = now.strftime("%H:%M:%S")
+#    spinner = Halo(text='Running', spinner={"interval": 80,	"frames": ["⠋","⠙","⠹","⠸","⠼","⠴","⠦","⠧","⠇","⠏"]})
+#    spinner.start()
 
     #for i in range(5):
     #    print("Running", "."*i, end="\r")
@@ -134,8 +144,8 @@ def test():
     #    myfile.write(f"Testing")
     print("\nhello")
 
-schedule.every(15).seconds.do(run_threaded, internet_on)
-#schedule.every(5).seconds.do(run_threaded, constant_check)
+#schedule.every(15).seconds.do(run_threaded, internet_on)
+schedule.every(5).seconds.do(run_threaded, constant_check)
 
 #schedule.every().day.at("00:00").do(run_threaded, status)
 #schedule.every(3).seconds.do(run_threaded, status)
@@ -156,13 +166,18 @@ schedule_test()
 
 print("Jobs:", schedule.get_jobs())
 #showtime()
-print("Start time: ", datetime.now())
+console.print("Start time: ", datetime.now(), style="magenta")
 #status()
 
-@Halo(text='Running', spinner={"interval": 80,	"frames": ["⠋","⠙","⠹","⠸","⠼","⠴","⠦","⠧","⠇","⠏"]})
-def run():
-    while True:
-        schedule.run_pending()
-        t.sleep(1)
+#@Halo(text='Running', spinner={"interval": 80,	"frames": ["⠋","⠙","⠹","⠸","⠼","⠴","⠦","⠧","⠇","⠏"]})
+def main():
+    with console.status("Running") as status:
+        while True:
+            schedule.run_pending()
+            t.sleep(1)
 
-run()
+
+
+if __name__ == "__main__":
+    main()
+
